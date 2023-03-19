92aa241321266372aaf8af1ad2b73d3d71b15ec1
Euan Ramsay <euancramsay@aol.com>
2023-03-19 19:05:45 +0000

    Add jest config for stryker

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/dist/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/.stryker-tmp/',
+  ],
   verbose: true,
   watchPathIgnorePatterns: ['<rootDir>/node_modules/'],
   collectCoverage: true,
